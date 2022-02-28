a4d2f0bdba24a7632554079235ebe0b918538a95
dannytszho <dannytszho@gmail.com>
2022-02-27 17:29:01 -0800

    Add sidebar

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Spotify 2.0</title>
+      </Head>
+
+      <main>
+        <Sidebar />
+      </main>
+    </div>
+  )
+}
+
+export default Home
